a3bd2b6865450c09cf70a75716d33cc20b856ccc
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-02-20 18:33:27 -0400

    Juntando o print e return em uma linha

--- collatz.py
+++ collatz.py
@@ -2,10 +2,9 @@
 def collatz(number):
 
     if number % 2 == 0:
-        print(number // 2)
-        return number // 2
+        return(print(number // 2))
+        
     else:
-        print(3 * number + 1)
-        return 3 * number + 1
+        return(print(3 * number + 1))
 number = input()
-collatz(int(number))
+collatz(int(number)) 
