9bb862a4c7cba7487b73da1f232536266087ce6f
Thiagoprepaz <profissionalcontactothiago@gmail.com>
2023-02-20 19:02:24 -0400

    Try to improve the code, second challenge

--- collatz.py
+++ collatz.py
@@ -1,10 +1,21 @@
+number = input()
 
 def collatz(number):
 
-    if number % 2 == 0:
-        return(print(number // 2))
-        
-    else:
-        return(print(3 * number + 1))
-number = input()
-collatz(int(number)) 
+        if number % 2 == 0:  # if the rest of the division is equal to 0 it's a even number
+            ver = number // 2
+            print(number // 2)
+            verification(int(ver))
+
+        else:  # if that's not a even, it only can be a odd
+            ver = 3 * number + 1
+            print(3 * number + 1)
+            verification(int(ver))
+
+def verification(ver):
+        if ver != 1:
+            collatz(number)
+        else:
+            return(print("You are done"))
+collatz(int(number))
+   
